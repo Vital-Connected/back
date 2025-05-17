2006657ddbcd5f4d747021033d417b893692ec40
Bianca Lorenzini <biancalorenzini.dev@gmail.com>
2025-05-17 12:16:36 -0300

    :sparkles: User Controller

---
+++ src/main/java/com/fatec/back/controller/UserController.java
@@ -0,0 +1,60 @@
+package com.fatec.back.controller;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.fatec.back.domain.User.User;
+import com.fatec.back.domain.User.UserRequestDTO;
+import com.fatec.back.service.UserService;
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+    @Autowired
+    private UserService service;
+
+    @GetMapping
+    public ResponseEntity<List<User>> getAll() {
+        return ResponseEntity.ok(service.getAllUsers());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<User> getById(@PathVariable Long id) {
+        Optional<User> user = service.getUserById(id);
+        return user.map(ResponseEntity::ok)
+                   .orElse(ResponseEntity.notFound().build());
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<User> update(@PathVariable Long id, @RequestBody UserRequestDTO body) {
+        Optional<User> updatedUser = service.updateUser(id, body);
+        return updatedUser.map(ResponseEntity::ok)
+                          .orElse(ResponseEntity.notFound().build());
+    }
+
+    @PutMapping("password/{id}")
+    public ResponseEntity<User> updatePassword(@PathVariable Long id, @RequestBody Map<String, Object> body) {
+        String newPassword = (String) body.get("password");
+        Long updatedBy = Long.valueOf(body.get("updated_by").toString());
+        Optional<User> updatedUser = service.updatePassword(id, newPassword, updatedBy);
+        return updatedUser.map(ResponseEntity::ok)
+                        .orElse(ResponseEntity.notFound().build());
+    }
+
+    @PutMapping("access/{id}")
+    public ResponseEntity<Void> toggleAccess(@PathVariable Long id) {
+        boolean deleted = service.toggleUserAccess(id);
+        return deleted ? ResponseEntity.noContent().build()
+                       : ResponseEntity.notFound().build();
+    }
+}
