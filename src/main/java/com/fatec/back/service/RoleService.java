516226ee2d7d4d319fe1e2a9a16b1a616e502669
Bianca Lorenzini <biancalorenzini.dev@gmail.com>
2025-05-17 11:11:31 -0300

    :sparkles: feat: Role Service

---
+++ src/main/java/com/fatec/back/service/RoleService.java
@@ -0,0 +1,49 @@
+package com.fatec.back.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.fatec.back.domain.Role.Role;
+import com.fatec.back.domain.Role.RoleDTO;
+import com.fatec.back.repository.RoleRepository;
+
+@Service
+public class RoleService {
+    @Autowired
+    private RoleRepository roleRepository;
+
+    public List<Role> getAllRoles(){
+        return roleRepository.findAll();
+    }
+
+    public Optional<Role> getRoleById(Long id){
+        return roleRepository.findById(id);
+    }
+
+    public Role createRole(RoleDTO dto) {
+        Role role = new Role();
+        role.setName(dto.name());
+        role.setDescription(dto.description());
+
+        return roleRepository.save(role);
+    }
+
+    public Optional<Role> updateRole(Long id, RoleDTO updatedData) {
+        return roleRepository.findById(id).map(existing -> {
+            existing.setName(updatedData.name());
+            existing.setDescription(updatedData.description());
+            return roleRepository.save(existing);
+        });
+    }
+
+    public boolean deleteRole(Long id) {
+        return roleRepository.findById(id).map(role -> {
+            role.setDeleted(!role.isDeleted());
+            roleRepository.save(role);
+            return true;
+        }).orElse(false);
+    }
+}
